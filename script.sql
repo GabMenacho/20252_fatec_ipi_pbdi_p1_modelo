6c95bc894dde36066ef144e5bcf5c489b4b0e2a4
Aluno <aluno@IPIRANGA.FATEC>
2025-10-07 10:49:30 -0300

    feat(p1): cria a tabela

--- script.sql
+++ script.sql
@@ -1,25 +1,8 @@
--- ----------------------------------------------------------------
--- 1 Base de dados e criação de tabela
---escreva a sua solução aqui
-
-
--- ----------------------------------------------------------------
--- 2 Cursor não vinculado (cálculo de preço médio)
---escreva a sua solução aqui
-
-
--- ----------------------------------------------------------------
--- 3 Cursor vinculado (Descrição mais longa)
---escreva a sua solução aqui
-
-
--- ----------------------------------------------------------------
--- 4 Armazenamento dos resultados
---escreva a sua solução aqui
-
-
--- ----------------------------------------------------------------
--- 5 Exportação dos dados
---escreva a sua solução aqui
-
--- ----------------------------------------------------------------
+DROP TABLE IF EXISTS tb_wine_reviews;
+CREATE TABLE tb_wine_reviews(
+     cod_wine SERIAL PRIMARY KEY,
+     country VARCHAR(200),
+     description VARCHAR(2000),
+     points INTEGER,
+     price FLOAT
+);
