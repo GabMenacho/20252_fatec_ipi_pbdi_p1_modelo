7b327d0393b3e0f0b1cbb6d1e2929e414c8ebfca
Gabriela Menacho <menacho.gabriela@gmail.com>
2025-10-11 23:55:42 -0300

    refactor(p1): reorganiza script de solucoes

--- script.sql
+++ script.sql
@@ -1,6 +1,79 @@
 -- ----------------------------------------------------------------
--- 5 Exportação dos dados
+-- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
+
+1 Base de dados e criação de tabela
+escreva a sua solução aqui
+
+CREATE TABLE tb_wine_reviews(
+     cod_wine SERIAL PRIMARY KEY,
+     country VARCHAR(200),
+     description VARCHAR(2000),
+     points INTEGER,
+     price FLOAT
+);
+
+-- ----------------------------------------------------------------
+-- 2 Cursor não vinculado (cálculo de preço médio)
+--escreva a sua solução aqui
+
+DO $$
+DECLARE
+    cur_paises_precos REFCURSOR; 
+    v_country TEXT;
+    v_avg_price NUMERIC(10, 2); 
+BEGIN 
+    OPEN cur_paises_precos FOR 
+        SELECT 
+            country, 
+            AVG(price)::NUMERIC(10, 2) 
+        FROM 
+            tb_wine_reviews 
+        WHERE 
+            country IS NOT NULL 
+        GROUP BY 
+            country 
+        ORDER BY 
+            country; 
+    LOOP 
+        FETCH cur_paises_precos INTO v_country, v_avg_price; 
+        EXIT WHEN NOT FOUND; 
+        RAISE NOTICE 'País: %, Preço Médio: %', v_country, v_avg_price;
+            -- INSERT INTO tb_price_and_description(country, description) VALUES (v_country, v_avg_price);
+    END LOOP; 
+    CLOSE cur_paises_precos; 
+END $$;
+
+-- ----------------------------------------------------------------
+-- 3 Cursor vinculado (Descrição mais longa)
+--escreva a sua solução aqui
+
+DO $$
+DECLARE
+    v_country TEXT;
+    v_longa_descricao TEXT;
+ 
+    cur_paises_descricoes CURSOR FOR
+        SELECT DISTINCT ON (country)
+            country,
+            description
+        FROM
+            tb_wine_reviews
+        WHERE
+            country IS NOT NULL AND description IS NOT NULL
+        ORDER BY
+            country, LENGTH(description) DESC;
+BEGIN
+    OPEN cur_paises_descricoes;
+ 
+    LOOP
+        FETCH cur_paises_descricoes INTO v_country, v_longa_descricao;
+        EXIT WHEN NOT FOUND;
+        RAISE NOTICE 'País: %, Descrição Mais Longa: %', v_country, v_longa_descricao;
+    END LOOP;
+ 
+    CLOSE cur_paises_descricoes;
+END $$;
 
 -------------------------------------------------------------
 -- 4 Armazenamento dos resultados
@@ -75,76 +148,7 @@ BEGIN
 END $$;
 
 -- ----------------------------------------------------------------
--- 3 Cursor vinculado (Descrição mais longa)
+-- 5 Exportação dos dados
 --escreva a sua solução aqui
-
-DO $$
-DECLARE
-    v_country TEXT;
-    v_longa_descricao TEXT;
- 
-    cur_paises_descricoes CURSOR FOR
-        SELECT DISTINCT ON (country)
-            country,
-            description
-        FROM
-            tb_wine_reviews
-        WHERE
-            country IS NOT NULL AND description IS NOT NULL
-        ORDER BY
-            country, LENGTH(description) DESC;
-BEGIN
-    OPEN cur_paises_descricoes;
- 
-    LOOP
-        FETCH cur_paises_descricoes INTO v_country, v_longa_descricao;
-        EXIT WHEN NOT FOUND;
-        RAISE NOTICE 'País: %, Descrição Mais Longa: %', v_country, v_longa_descricao;
-    END LOOP;
- 
-    CLOSE cur_paises_descricoes;
-END $$;
-
-
+-- arquivo tb_price_and_description.csv
 -- ----------------------------------------------------------------
--- 2 Cursor não vinculado (cálculo de preço médio)
---escreva a sua solução aqui
-
-DO $$
-DECLARE
-    cur_paises_precos REFCURSOR; 
-    v_country TEXT;
-    v_avg_price NUMERIC(10, 2); 
-BEGIN 
-    OPEN cur_paises_precos FOR 
-        SELECT 
-            country, 
-            AVG(price)::NUMERIC(10, 2) 
-        FROM 
-            tb_wine_reviews 
-        WHERE 
-            country IS NOT NULL 
-        GROUP BY 
-            country 
-        ORDER BY 
-            country; 
-    LOOP 
-        FETCH cur_paises_precos INTO v_country, v_avg_price; 
-        EXIT WHEN NOT FOUND; 
-        RAISE NOTICE 'País: %, Preço Médio: %', v_country, v_avg_price;
-            -- INSERT INTO tb_price_and_description(country, description) VALUES (v_country, v_avg_price);
-    END LOOP; 
-    CLOSE cur_paises_precos; 
-END $$;
-
--- ----------------------------------------------------------------
--- 1 Base de dados e criação de tabela
--- escreva a sua solução aqui
-
--- CREATE TABLE tb_wine_reviews(
---      cod_wine SERIAL PRIMARY KEY,
---      country VARCHAR(200),
---      description VARCHAR(2000),
---      points INTEGER,
---      price FLOAT
--- );
